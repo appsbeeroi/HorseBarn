694242488ab41cc32c9626efebf119a2ea6b0b10
esplusnin <esplusnin94@yandex.ru>
2025-09-26 08:19:44 +0300

    Initial Commit

---
+++ HorseBarn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HorseBarn
+//
+//  Created by Евгений on 26.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HorseBarn/HorseBarnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseBarnApp.swift
+//  HorseBarn
+//
+//  Created by Евгений on 26.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct HorseBarnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
